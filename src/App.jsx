4848508e0ca8ca17b1888e2860a1013121da8f7e
Nandini Dahal <bidishadahal338@gmail.com>
2026-04-02 13:39:22 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import Home  from './Pages/blog/Home';
+import Register from './Pages/auth/Register';
+import  AddBlog  from './Pages/blog/AddBlog';
+
+import Login from './Pages/auth/Login.jsx';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/addblog" element={<AddBlog />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App
---
+++ src/Pages/auth/Register.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+       <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <form className="w-full max-w-md p-6 rounded-2xl shadow-md">
+        <h1 className="text-2xl font-bold mb-6 text-center text-black/100 ">Register here to continue...</h1>
+
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="email"
+            name="floating_email"
+            id="floating_email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="floating_email"
+            className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Email address
+          </label>
+        </div>
+
+          <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="floating_username"
+            id="floating_username"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="Username"
+            className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Username
+          </label>
+        </div>
+
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="password"
+            name="floating_password"
+            id="floating_password"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="floating_password"
+            className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Password
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm w-full px-5 py-2.5"
+        >
+          Register
+        </button>
+      </form>
+    </div>
+    </div>
+  )
+}
+
+export default Register
